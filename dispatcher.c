689dec0acf0648b8d810a010f88c81a55233962e
Daniel-Ems <danielems3@outlook.com>
2017-05-13 09:10:12 -0400

    clean code and variablel names

--- dispatcher.c
+++ dispatcher.c
@@ -21,47 +21,44 @@
 #define SOCK_PATH "echo_socket"
 
 //Needs to be volatile because it is affected outside the program. 
-
-volatile int what;
+volatile int remote_fd;
 volatile bool exit_run;
 volatile pthread_t fgets_thread;
-char str[4096];
+char str[4096]; // not currently using
 char buf[4096];
-int file_array[100];
-int i = 0;
-int s;
+int fd_array[100];
+int i = 0;//array counter
+int socket_fd;
 
 //Allows for proper exit.
 void signal_handler(int signal_name)
 {
-        close(what);
-        printf("closed appropriately\n");
-        exit(1); 
+        shutdown(socket_fd, SHUT_RDWR);
+        close(socket_fd);
+        exit(1);
 }
 
 
 void *fgets_function(void *arg)
 {
-    int *value;
-    value = (int*)1;
+
     for(;;)
     {
         if(fgets(buf, sizeof(buf), stdin)==NULL)
         {
-           shutdown(s, SHUT_RDWR);
-           //close(s);
+           shutdown(socket_fd, SHUT_RDWR);
            pthread_exit(NULL);
         }
         else
         {
             for(int a = 0; a <= i; a++)
             { 
-                send(file_array[a], buf, strlen(buf) +1, 0);
+                send(fd_array[a], buf, strlen(buf) +1, 0);
             }
         }
     }
-    close(what);
-    close(s);
+    close(remote_fd);
+    close(socket_fd);
     exit(1);
 }
 
@@ -76,65 +73,68 @@ sigs.sa_handler = signal_handler;
 sigs.sa_flags = 0;
 sigemptyset(&sigs.sa_mask);
 
+
+
+
+
+struct sockaddr_in sock_struct, rem_struct;
+
+sock_struct.sin_family = AF_INET;
+
 int d_ip;
-int t;
-int rc;
-int sc;
-int fg;
-int *value;
-
-
-struct sockaddr_in sa, remote;
-
-sa.sin_family = AF_INET;
 inet_pton(AF_INET, "127.0.0.1", &d_ip);
-sa.sin_addr.s_addr = d_ip;
-sa.sin_port  = htons(51236);
-remote.sin_family = AF_INET;
+sock_struct.sin_addr.s_addr = d_ip;
+sock_struct.sin_port  = htons(51236);
+rem_struct.sin_family = AF_INET;
 
     if(sigaction(SIGINT, &sigs, NULL) == -1) {
         perror("sigaction");
         exit(1);
     }
     
-    if((s = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    if((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("socket");
         exit(1);
     }
 
-    if((bind(s, (struct sockaddr *)&sa, sizeof(sa))) == -1)
+    if((bind(socket_fd, (struct sockaddr *)&sock_struct, sizeof(sock_struct))) == -1)
     {
         perror("bind");
         exit(1);
     }
 
-    if(listen(s, 100) == -1)
+    if(listen(socket_fd, 100) == -1)
     {
         perror("listen");
         exit(1);
     }
 
     printf("Waiting for a connection ..\n");
-    fg = pthread_create((pthread_t *)&fgets_thread, NULL, fgets_function,(void *)&s);
+
+    int t_retval;
+    t_retval = pthread_create((pthread_t *)&fgets_thread, NULL, fgets_function,(void *)&socket_fd);
+
+    int rs_size;
     for(;;)
     { 
-        t = sizeof(remote);
+        rs_size = sizeof(rem_struct);
         
-        if ((what = accept(s, (struct sockaddr *)&remote, &t)) == -1)
+        if ((remote_fd = accept(socket_fd, (struct sockaddr *)&rem_struct, &rs_size)) == -1)
         {
             pthread_join(fgets_thread, NULL);
+            printf("I joined\n");
             break;
         }
 
-        file_array[i] = what;
+        fd_array[i] = remote_fd;
 
         i++;
 
         printf("Connected. \n");
     }
-    //close(s);
-    close(s);
+
+    close(socket_fd);
     return 0;
 
 }
