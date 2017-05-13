c52b4a4842f78c8725a10450eae59dc63986672d
Daniel-Ems <danielems3@outlook.com>
2017-05-13 10:39:52 -0400

    comment and update variable names

--- dispatcher.c
+++ dispatcher.c
@@ -18,93 +18,108 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-#define SOCK_PATH "echo_socket"
+enum port{ 
+           //The port being used. Changes must be reflected in listener.c
+           PORT = 51236,
+           //MAX buffer sie for fgets
+           MAX_BUF = 4096,
+           //Maximum number of connections allowed. 
+           MAX_CONNECT = 100,
+         };
 
-//Needs to be volatile because it is affected outside the program. 
-volatile int remote_fd;
-volatile bool exit_run;
+//Needs to be volatile. Could be affected from outside the program. 
+volatile int server_fd;
 volatile pthread_t fgets_thread;
-char str[4096]; // not currently using
-char buf[4096];
-int fd_array[100];
-int i = 0;//array counter
-int socket_fd;
+volatile int client_fd;
 
-//Allows for proper exit.
+//Stores input from fgets
+char buf[MAX_BUF];
+
+//holds file descriptors from accept()
+int fd_array[MAX_CONNECT];
+int i = 0; //array counter
+
+//client file descriptors
+//int client_fd;
+
+//Allows for proper handling of Sigint(str-c).
 void signal_handler(int signal_name)
 {
-        shutdown(socket_fd, SHUT_RDWR);
-        close(socket_fd);
+        shutdown(server_fd, SHUT_RDWR);
+        close(server_fd);
         exit(1);
 }
 
-
+//Threaded to handle user input.
 void *fgets_function(void *arg)
 {
-
     for(;;)
     {
+        //If recieves EOT: send shutdown signals and exit thread.
         if(fgets(buf, sizeof(buf), stdin)==NULL)
         {
-           shutdown(socket_fd, SHUT_RDWR);
+           shutdown(server_fd, SHUT_RDWR);
            pthread_exit(NULL);
         }
         else
         {
+            //Send message to all file_descriptors in fd_array
             for(int a = 0; a <= i; a++)
             { 
                 send(fd_array[a], buf, strlen(buf) +1, 0);
             }
         }
     }
-    close(remote_fd);
-    close(socket_fd);
-    exit(1);
 }
 
     
 int main(void)
 {
 
-//sigaction struct  to handle sigint. 
-struct sigaction sigs;
+    //sigaction struct to handle sigint. 
+    struct sigaction sigs;
 
-sigs.sa_handler = signal_handler;
-sigs.sa_flags = 0;
-sigemptyset(&sigs.sa_mask);
+    sigs.sa_handler = signal_handler; //function to handle signals
+    sigs.sa_flags = 0;
+    sigemptyset(&sigs.sa_mask);
 
-
-
-
-
-struct sockaddr_in sock_struct, rem_struct;
-
-sock_struct.sin_family = AF_INET;
-
-int d_ip;
-inet_pton(AF_INET, "127.0.0.1", &d_ip);
-sock_struct.sin_addr.s_addr = d_ip;
-sock_struct.sin_port  = htons(51236);
-rem_struct.sin_family = AF_INET;
 
     if(sigaction(SIGINT, &sigs, NULL) == -1) {
         perror("sigaction");
         exit(1);
     }
     
-    if((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    //Declare and assign values to structs necessary for sockets. This is were
+    //communication protocols(AF_INET), addresses, and ports are assigned. 
+    struct sockaddr_in server_struct;
+    server_struct.sin_family = AF_INET; //IPV4
+    int d_ip;
+    inet_pton(AF_INET, "127.0.0.1", &d_ip); //loopback
+    server_struct.sin_addr.s_addr = d_ip;
+    server_struct.sin_port  = htons(PORT); //port listening on
+
+    struct sockaddr_in client_struct; //client socket
+    client_struct.sin_family = AF_INET;
+
+    //Creates socket file descriptor. AF_INET(IPv4), and sock stream are used 
+    //to allow for reliable tcp connections.
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("socket");
         exit(1);
     }
 
-    if((bind(socket_fd, (struct sockaddr *)&sock_struct, sizeof(sock_struct))) == -1)
+    //Assigns address to socket. Otherwise the socket previously created
+    //would live in name space.
+    if((bind(server_fd, (struct sockaddr *)&server_struct, 
+                         sizeof(server_struct))) == -1)
     {
         perror("bind");
         exit(1);
     }
 
-    if(listen(socket_fd, 100) == -1)
+    //Makes the socket passive(listens for connections using accept();
+    if(listen(server_fd, 100) == -1)
     {
         perror("listen");
         exit(1);
@@ -113,28 +128,36 @@ rem_struct.sin_family = AF_INET;
     printf("Waiting for a connection ..\n");
 
     int t_retval;
-    t_retval = pthread_create((pthread_t *)&fgets_thread, NULL, fgets_function,(void *)&socket_fd);
-
+    //Thread fgets in order to allow a fgets to constantly be able to revieve
+    //input and send to file descriptors in fd_array.
+    t_retval = pthread_create((pthread_t *)&fgets_thread, NULL, fgets_function,
+                                                            (void *)&server_fd);
+    //used for readability in accept.
     int rs_size;
+
+    //loop is necessary to continue entering into accept blocks.
     for(;;)
     { 
-        rs_size = sizeof(rem_struct);
+        //used for multiple fds
+        rs_size = sizeof(client_struct);
         
-        if ((remote_fd = accept(socket_fd, (struct sockaddr *)&rem_struct, &rs_size)) == -1)
+        //In condidtional to allow for breaks in accept blocks.
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_struct, 
+                                                            &rs_size)) == -1)
         {
+            //If the accept block is broke, clean up and exit. 
             pthread_join(fgets_thread, NULL);
-            printf("I joined\n");
             break;
         }
 
-        fd_array[i] = remote_fd;
+        fd_array[i] = client_fd;
 
         i++;
 
         printf("Connected. \n");
     }
-
-    close(socket_fd);
+    //properly close the socket. 
+    close(server_fd);
     return 0;
 
 }
